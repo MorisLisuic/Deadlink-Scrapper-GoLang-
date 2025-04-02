b7055b918fb149624dfb3bdaf16e2b1f77140585
Lisnic Marius <lisnicmarius2000@gmail.com>
2025-04-03 00:53:51 +0300

    made it work for non JS rendered pages

--- main.go
+++ main.go
@@ -19,14 +19,14 @@ func main() {
 	fmt.Println("Enter commands (type 'exit' to quit):")
 
 	for {
-		fmt.Print("> ") // Prompt for input
-		scanner.Scan()  // Read user input
+		fmt.Print("> ")
+		scanner.Scan()
 		input := strings.TrimSpace(scanner.Text())
 		words := strings.Fields(input)
+		if words[0] == "exit" {
+			break
+		}
 		switch words[0] {
-		case "exit":
-			fmt.Println("Exiting program...")
-			break // Stop the loop
 		case "help":
 			fmt.Println("Usage:")
 			fmt.Println("scrape <url>")
@@ -50,11 +50,9 @@ type Crawler struct {
 	results chan string
 }
 
-// Fetches a page, extracts internal links, and recursively crawls
 func (c *Crawler) crawl(targetURL string) {
 	defer c.wg.Done()
 
-	// Mark URL as visited
 	c.mu.Lock()
 	if c.visited[targetURL] {
 		c.mu.Unlock()
@@ -63,7 +61,6 @@ func (c *Crawler) crawl(targetURL string) {
 	c.visited[targetURL] = true
 	c.mu.Unlock()
 
-	// Check link status
 	resp, err := http.Get(targetURL)
 	if err != nil {
 		c.results <- fmt.Sprintf("%s - ERROR: %s", targetURL, err)
@@ -73,27 +70,24 @@ func (c *Crawler) crawl(targetURL string) {
 	if resp.StatusCode != 200 {
 		c.results <- fmt.Sprintf("%s - %d %s", targetURL, resp.StatusCode, http.StatusText(resp.StatusCode))
 	}
-	// Only parse HTML pages
+
 	if !strings.HasPrefix(resp.Header.Get("Content-Type"), "text/html") {
 		c.results <- fmt.Sprintf("%s - %s", targetURL, "", "Is not html")
 		return
 	}
 	c.results <- fmt.Sprintf("%s - %d %s", targetURL, resp.StatusCode, http.StatusText(resp.StatusCode))
 
-	// Parse links from HTML
 	links, err := extractInternalLinks(targetURL, resp)
 	if err != nil {
 		return
 	}
 
-	// Crawl discovered links
 	for _, link := range links {
 		c.wg.Add(1)
 		go c.crawl(link)
 	}
 }
 
-// Extracts internal links from an HTML document
 func extractInternalLinks(baseURL string, resp *http.Response) ([]string, error) {
 	var links []string
 	parsedBase, _ := url.Parse(baseURL)
@@ -126,12 +120,6 @@ func extractInternalLinks(baseURL string, resp *http.Response) ([]string, error)
 	return links, nil
 }
 
-// Checks if the response is an HTML page
-func isHTML(resp *http.Response) bool {
-	contentType := resp.Header.Get("Content-Type")
-	return contentType == "text/html" || contentType == "text/html; charset=UTF-8"
-}
-
 func handleUrl(startUrl string) {
 	//startUrl := "https://scrape-me.dreamsofcode.io" // Change this to the target website
 
@@ -142,17 +130,14 @@ func handleUrl(startUrl string) {
 
 	fmt.Println("Starting crawl...")
 
-	// Start crawling
 	crawler.wg.Add(1)
 	go crawler.crawl(startUrl)
 
-	// Goroutine to close results channel when done
 	go func() {
 		crawler.wg.Wait()
 		close(crawler.results)
 	}()
 
-	// Print results as they arrive
 	for result := range crawler.results {
 		fmt.Println(result)
 	}
